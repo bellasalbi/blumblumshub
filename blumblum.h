15f861ff1449a7965a54e9d2bf52b8a487537cfb
kadamczyk-i2a <kadamczyk@i2asolutions.com>
2014-10-28 19:07:11 +0100

    Nowy commit

--- blumblum.h
+++ blumblum.h
@@ -12,6 +12,7 @@
 #include <ctime>
 #include <time.h>
 #include <math.h>
+#include <iomanip>
 
 using namespace std;
 
@@ -25,6 +26,12 @@ public:
 	int czas_w_sekundach();
 	void swichthem(int &, int &);
 	void wypisz_n(int );
+	int test_zero_jeden(int);
+	void test_zero_jeden_UI();
+	int test_bertrand_katy(int);
+	void test_bertrand_katy_UI();
+	int test_monte_carlo(int,int);
+	void test_monte_carlo_UI();
 private:
 	unsigned long int stanX; //dla init - pierwszy stan
 	unsigned long int stanX_1; //stany kolejne
